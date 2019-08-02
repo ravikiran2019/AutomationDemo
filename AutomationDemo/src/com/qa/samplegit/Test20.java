feb9f08ba7be6830a8871bee2c3dc999de6fddbf
JK <JK@LAPTOP-FN8U3CV9>
2019-08-02 09:15:15 +0530

    code has been added

--- AutomationDemo/src/com/qa/samplegit/Test20.java
+++ AutomationDemo/src/com/qa/samplegit/Test20.java
@@ -1,5 +1,11 @@
 package com.qa.samplegit;
 
 public class Test20 {
+	
 
+public static void main()
+{
+	System.out.println("hi");
+
+}
 }
---
+++ AutomationDemo/src/com/qa/samplegit/Test21.java
@@ -0,0 +1,11 @@
+package com.qa.samplegit;
+
+public class Test21 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hi");
+
+	}
+
+}
